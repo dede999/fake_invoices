49acbbc100e1a52335efab5038ea75fe3eb17c71
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-08-03 23:16:47 -0300

    Implement main function with the existing pieces

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"invoices_print/lib"
 	"time"
 )
 
 func main() {
+	defer lib.RecoverFromPanic()
+
+	parallelFlag := flag.Bool("parallel", false, "Will it be run in parallel mode? (default: FALSE)")
+	invoiceItems := flag.Int("items", 20, "The number of invoices to generate")
+	projects := flag.Int("projects", 1, "The number of projects to generate")
 	flag.Parse()
+	lib.DisplayOptions(parallelFlag, invoiceItems, projects)
+	start := time.Now()
+	if *parallelFlag {
+		lib.AsyncRun(invoiceItems, projects)
+	} else {
+		lib.SyncRun(invoiceItems, projects)
+	}
 	fmt.Println("Took:", time.Since(start))
 }
